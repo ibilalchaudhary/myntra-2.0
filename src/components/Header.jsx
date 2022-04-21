72efb6467a996ae8482f6c00d94e644b00aa8c42
ibilalchaudhary <bilalchaudhary478@gmail.com>
2022-04-21 17:50:32 +0500

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
+import user from "../assets/user.svg";
+import heart from "../assets/heart.svg";
+import cart from "../assets/cart.svg";
 
 function NavLink({ title, to, list }) {
   return (
@@ -68,11 +71,10 @@ export default function Header() {
                 stroke-width="2"
                 stroke-linecap="round"
                 stroke-linejoin="round"
-                class="feather feather-menu"
+                class="feather feather-x"
               >
-                <line x1="3" y1="12" x2="21" y2="12"></line>
-                <line x1="3" y1="6" x2="21" y2="6"></line>
-                <line x1="3" y1="18" x2="21" y2="18"></line>
+                <line x1="18" y1="6" x2="6" y2="18"></line>
+                <line x1="6" y1="6" x2="18" y2="18"></line>
               </svg>
             ) : (
               <svg
@@ -85,10 +87,11 @@ export default function Header() {
                 stroke-width="2"
                 stroke-linecap="round"
                 stroke-linejoin="round"
-                class="feather feather-x"
+                class="feather feather-menu"
               >
-                <line x1="18" y1="6" x2="6" y2="18"></line>
-                <line x1="6" y1="6" x2="18" y2="18"></line>
+                <line x1="3" y1="12" x2="21" y2="12"></line>
+                <line x1="3" y1="6" x2="21" y2="6"></line>
+                <line x1="3" y1="18" x2="21" y2="18"></line>
               </svg>
             )}
           </button>
@@ -98,6 +101,12 @@ export default function Header() {
                 title="Men"
                 to="/"
                 list={[
+                  {
+                    title: "LOREM IPSUM",
+                    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim.",
+                    to: "/",
+                  },
+
                   {
                     title: "LOREM IPSUM",
                     info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim.",
@@ -170,11 +179,7 @@ export default function Header() {
                     info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim.",
                     to: "/",
                   },
-                  {
-                    title: "LOREM IPSUM",
-                    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim.",
-                    to: "/",
-                  },
+
                   {
                     title: "LOREM IPSUM",
                     info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim.",
@@ -267,6 +272,16 @@ export default function Header() {
                 title="Home & Living"
                 to="/"
                 list={[
+                  {
+                    title: "LOREM IPSUM",
+                    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim.",
+                    to: "/",
+                  },
+                  {
+                    title: "LOREM IPSUM",
+                    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim.",
+                    to: "/",
+                  },
                   {
                     title: "LOREM IPSUM",
                     info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim.",
@@ -434,7 +449,84 @@ export default function Header() {
             </div>
           ) : null}
         </div>
-        <div className="header__wrapper__right">sadasd</div>
+        <div className="header__wrapper__right">
+          <div className="header__wrapper__right__input__wrapper">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-search"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+            </svg>
+            <input
+              type="text"
+              placeholder="Search for product, Brand and more"
+              className="header__wrapper__right__input"
+            />
+          </div>
+          <div className="header__wrapper__right__entry">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-user"
+            >
+              <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle>
+            </svg>
+            <span> User</span>
+          </div>
+          <div className="header__wrapper__right__entry">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-heart"
+            >
+              <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+            </svg>
+            <span> Wishlist</span>
+          </div>
+          <div className="header__wrapper__right__entry">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-shopping-cart"
+            >
+              <circle cx="9" cy="21" r="1"></circle>
+              <circle cx="20" cy="21" r="1"></circle>
+              <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+            </svg>
+            <span> Bag</span>
+          </div>
+        </div>
       </div>
     </div>
   );
